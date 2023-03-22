2c6c0ef5cf0781c8f60d99fb63ccf8c6f3ffaf21
JHFVR <jonathan1@gmx.de>
2023-03-22 21:37:34 +0100

    Main merge in changes from new env

--- robot_control/main.py
+++ robot_control/main.py
@@ -1,4 +1,3 @@
-# main.py -- put your code here!
 from arm_6axis import Arm_6
 import time
 PIN_LIST = [16, 17, 18, 19, 20, 21]
@@ -15,3 +14,14 @@ arm.set_single_angle (4,0)
 arm.set_single_angle (5,0)
 arm.flesh ()
 print(arm.pos_report())
+
+arm.set_single_angle (1,-20); arm.flesh()
+arm.set_single_angle (2,30); arm.flesh()
+arm.set_single_angle (3,-30); arm.flesh()
+arm.set_single_angle (4,-90); arm.flesh()
+
+time.sleep(10)
+
+arm.set_single_angle (4,0); arm.flesh()
+arm.set_single_angle (3,-50); arm.flesh()
+arm.set_single_angle (2, 74); arm.flesh()
